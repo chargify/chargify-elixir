ad2a679ef638475994d284da7b15eb8617231875
Eric Farkas <eric@prudentiadigital.com>
2016-08-31 17:23:41 -0400

    WIP: remote test

---
+++ test/remote_product_test.exs
@@ -0,0 +1,39 @@
+defmodule RemoteProductTest do
+  use ExUnit.Case
+
+  @tag :remote
+  test "foo" do
+    {:created, product_family} = Chargify.ProductFamilies.create(%{
+      "name" => "Acme Online", 
+      "handle" => "acme-online",
+      "description" => "A product family",
+    })
+    
+    product_family_id = product_family["id"]
+
+    Chargify.Products.create(%{
+      "name" => "Pro Plan", 
+      "product_family_id" => product_family_id, 
+      "interval_unit" => "month", 
+      "interval" => 1, 
+      "price_in_cents" => 9900
+    })
+    assert %{
+      "name" => "Pro Plan",
+      "product_family" => %{
+        "id" => product_family_id,
+      },
+      "interval_unit" => "month",
+      "interval" => 1,
+      "price_in_cents" => 9900
+    } = product
+    
+    id = product["id"]
+
+    {:ok, products} = Chargify.Products.list(per_page: 1)
+    assert hd(products)["id"] == id
+
+    {:ok, product} = Chargify.Products.get(id)
+    assert product["id"] == id
+  end
+end
