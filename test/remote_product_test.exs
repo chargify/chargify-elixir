b65307bf7bf28691222845fcc7c328730e4a7692
Christopher Moeller <cmoel@me.com>
2016-08-31 19:03:38 -0400

    Extract setup functions

--- test/remote_product_test.exs
+++ test/remote_product_test.exs
@@ -6,13 +6,33 @@ defmodule RemoteProductTest do
     product_family_name = UUID.uuid4(:hex)
     product_name        = UUID.uuid4(:hex)
 
+    id = product_family_name
+    |> create_product_family
+    |> create_product(product_name)
+
+    {:ok, products} = Chargify.Products.list
+    assert products
+      |> Enum.map(&(Map.get(&1, "id")))
+      |> Enum.member?(id)
+
+    {:ok, product} = Chargify.Products.get(id)
+    assert product["id"] == id
+    refute product["archived_at"]
+
+    {:ok, product} = Chargify.Products.archive(id)
+    assert product["id"] == id
+    assert product["archived_at"]
+  end
+
+  defp create_product_family(product_family_name) do
     {:created, product_family} = Chargify.ProductFamilies.create(%{
       "name" => product_family_name,
       "description" => "A product family",
     })
+    Map.get(product_family, "id")
+  end
 
-    product_family_id = product_family["id"]
-
+  defp create_product(product_family_id, product_name) do
     {:created, product} = Chargify.Products.create(%{
       "name" => product_name,
       "product_family_id" => product_family_id,
@@ -29,20 +49,6 @@ defmodule RemoteProductTest do
       "interval" => 1,
       "price_in_cents" => 9900
     } = product
-
-    id = product["id"]
-
-    {:ok, products} = Chargify.Products.list
-    assert products
-      |> Enum.map(&(Map.get(&1, "id")))
-      |> Enum.member?(id)
-
-    {:ok, product} = Chargify.Products.get(id)
-    assert product["id"] == id
-    refute product["archived_at"]
-
-    {:ok, product} = Chargify.Products.archive(id)
-    assert product["id"] == id
-    assert product["archived_at"]
+    Map.get(product, "id")
   end
 end
